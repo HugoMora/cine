035074b3cd02e01e3f1953b2454606e665e7aade
Carlos Bueno <carlos.b.bustios@gmail.com>
2019-11-10 12:49:29 -0500

    forms 2

--- apps/cine/views.py
+++ apps/cine/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
 from django.shortcuts import render, redirect, get_object_or_404
